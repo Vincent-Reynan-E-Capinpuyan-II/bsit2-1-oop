a208bab6f425d54636393c4ec1efce37f8d14323
Vincent Reynan E. Capinpuyan II <vcapinpuyan32170@liceo.edu.ph>
2025-08-04 22:43:03 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+    public static void main (String[] args){
+
+        Book book1 = new Book("Platform Technology", "Ernesto Razo", 150, true);
+
+
     }
 }

889ad53f9f3faf874f4420411c6853e2a031e009
Vincent Reynan E. Capinpuyan II <vcapinpuyan32170@liceo.edu.ph>
2025-08-07 10:45:11 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,40 +6,30 @@ public class Main {
 
         int passingCount = 0;
 
-        // Student 1
         s1.displayInfo();
         double avg1 = s1.calculateAverage();
-        String letter1 = s1.getLetterGrade();
-        boolean isPass1 = s1.isPassing();
         System.out.printf("Average Grade : %.2f\n", avg1);
-        System.out.println("Letter Grade  : " + letter1);
-        System.out.println("Status        : " + (isPass1 ? "PASSING" : "FAILING"));
+        System.out.println("Letter Grade  : " + s1.getLetterGrade());
+        System.out.println("Status        : " + (s1.isPassing() ? "PASSING" : "FAILING"));
         System.out.println();
-        if (isPass1) passingCount++;
+        if (s1.isPassing()) passingCount++;
 
-        // Student 2
         s2.displayInfo();
         double avg2 = s2.calculateAverage();
-        String letter2 = s2.getLetterGrade();
-        boolean isPass2 = s2.isPassing();
         System.out.printf("Average Grade : %.2f\n", avg2);
-        System.out.println("Letter Grade  : " + letter2);
-        System.out.println("Status        : " + (isPass2 ? "PASSING" : "FAILING"));
+        System.out.println("Letter Grade  : " + s2.getLetterGrade());
+        System.out.println("Status        : " + (s2.isPassing() ? "PASSING" : "FAILING"));
         System.out.println();
-        if (isPass2) passingCount++;
+        if (s2.isPassing()) passingCount++;
 
-        // Student 3
         s3.displayInfo();
         double avg3 = s3.calculateAverage();
-        String letter3 = s3.getLetterGrade();
-        boolean isPass3 = s3.isPassing();
         System.out.printf("Average Grade : %.2f\n", avg3);
-        System.out.println("Letter Grade  : " + letter3);
-        System.out.println("Status        : " + (isPass3 ? "PASSING" : "FAILING"));
+        System.out.println("Letter Grade  : " + s3.getLetterGrade());
+        System.out.println("Status        : " + (s3.isPassing() ? "PASSING" : "FAILING"));
         System.out.println();
-        if (isPass3) passingCount++;
+        if (s3.isPassing()) passingCount++;
 
-        // Summary
         System.out.println("========");
         System.out.println("Total Students Passing: " + passingCount);
         System.out.println("========");

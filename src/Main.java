d28f22dfc77ab9587c2c5fa103394cf4bf4aaedd
Vincent Reynan E. Capinpuyan II <vcapinpuyan32170@liceo.edu.ph>
2025-08-19 14:44:57 +0800

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+    public static void main(String[] args) {
+        GradeCalculator gc = new GradeCalculator();
+
+        String studentName = "John Smith";
+        double average = gc.calculateAverage(85.5, 92.0, 78.5, 90.0);
+        String letterGrade = gc.getLetterGrade(average);
+
+        gc.displayResult(studentName, average);
+        gc.displayResult(studentName, average, letterGrade);
+        }
     }
-}

5ac92d0e1300a4e2a786a7047651738e5e0f9078
unknown <vcapinpuyan32170@liceo.edu.ph>
2025-10-02 11:00:31 +0800

    semi-final-exam

---
+++ src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+    private String department = "";
+    private String major = "";
+
+    public Student (String userId, String name, String email, String studentId, String major){
+        super(userID, name, email)
+    }
+}
